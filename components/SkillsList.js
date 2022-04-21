083c9f5a50196bb1b17a3c25c06f4ef90c62ea47
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-04-21 17:05:53 +0530

    Add framer motion

--- components/SkillsList.js
+++ components/SkillsList.js
@@ -1,34 +1,26 @@
-import {
-    SiReact,
-    SiNextdotjs,
-    SiExpress,
-    SiSelenium,
-    SiMysql,
-    SiMongodb,
-    SiTailwindcss,
-    SiBootstrap,
-    SiGit,
-    SiJava,
-    SiJavascript
-} from "react-icons/si";
+import { skillsData } from "../utils/skillUtil";
 import SkillItem from "./SkillItem";
+import { AnimatePresence, motion } from "framer-motion";
 
 const SkillsList = () => {
     return (
-        <section className='mx-5 my-2' id="skills">
-            <h2 className='text-2xl font-bold mb-8 text-center lg:text-left'>Skills</h2>
+        <section className='mx-5 my-2' id='skills'>
+            <h2 className='text-2xl font-bold mb-8 text-center lg:text-left'>
+                Skills
+            </h2>
             <div className='grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-10 px-3 lg:px-0'>
-                <SkillItem Icon={SiJava} title='Java' />
-                <SkillItem Icon={SiSelenium} title='Selenium' />
-                <SkillItem Icon={SiJavascript} title='Javascript' />
-                <SkillItem Icon={SiReact} title='React' />
-                <SkillItem Icon={SiNextdotjs} title='NextJS' />
-                <SkillItem Icon={SiExpress} title='Express' />
-                <SkillItem Icon={SiMysql} title='MySql' />
-                <SkillItem Icon={SiMongodb} title='MongoDB' />
-                <SkillItem Icon={SiTailwindcss} title='TailwindCSS' />
-                <SkillItem Icon={SiBootstrap} title='Bootstrap' />
-                <SkillItem Icon={SiGit} title='Git/Github' />
+                <AnimatePresence>
+                    {skillsData.map(({ Icon, title }, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <SkillItem Icon={Icon} title={title} />
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
             </div>
         </section>
     );
---
+++ utils/skillUtil.js
@@ -0,0 +1,60 @@
+import {
+    SiReact,
+    SiNextdotjs,
+    SiExpress,
+    SiSelenium,
+    SiMysql,
+    SiMongodb,
+    SiTailwindcss,
+    SiBootstrap,
+    SiGit,
+    SiJava,
+    SiJavascript,
+} from "react-icons/si";
+
+export const skillsData = [
+    {
+        Icon: SiJava,
+        title: "Java",
+    },
+    {
+        Icon: SiSelenium,
+        title: "Selenium",
+    },
+    {
+        Icon: SiJavascript,
+        title: "Javascript",
+    },
+    {
+        Icon: SiReact,
+        title: "React",
+    },
+    {
+        Icon: SiNextdotjs,
+        title: "NextJS",
+    },
+    {
+        Icon: SiExpress,
+        title: "Express",
+    },
+    {
+        Icon: SiMysql,
+        title: "MySql",
+    },
+    {
+        Icon: SiMongodb,
+        title: "MongoDB",
+    },
+    {
+        Icon: SiTailwindcss,
+        title: "TailwindCSS",
+    },
+    {
+        Icon: SiBootstrap,
+        title: "Bootstrap",
+    },
+    {
+        Icon: SiGit,
+        title: "Git/Github",
+    },
+];
