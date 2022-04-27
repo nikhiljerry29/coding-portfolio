95ee696c3c00db238f0de52935dd7bbb7df782c8
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-04-27 18:00:48 +0530

    Add mobile screen navigation

--- components/ExperienceList.js
+++ components/ExperienceList.js
@@ -1,9 +1,11 @@
 import ExperienceItem from "./ExperienceItem";
 
-export default function ExperienceList({ experienceData }) {
+const ExperienceList = ({ experienceData }) => {
     return (
         <section id='experience' className='mx-5 py-2 lg:pt-24'>
-            <h2 className='text-2xl font-semibold mb-8 text-center lg:text-left'>Work Experience</h2>
+            <h2 className='text-2xl font-semibold mb-8 text-center lg:text-left'>
+                Work Experience
+            </h2>
 
             {experienceData.map(
                 ({ title, company, startDate, endDate }, index) => (
@@ -18,4 +20,5 @@ export default function ExperienceList({ experienceData }) {
             )}
         </section>
     );
-}
+};
+export default ExperienceList;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,39 +1,51 @@
+import {
+    HiOutlineBriefcase, HiOutlineChatAlt2,
+    HiOutlineCode, HiOutlineDocumentDuplicate, HiOutlineIdentification
+} from "react-icons/hi";
+
 const Navbar = () => {
     return (
-        <nav className='p-5 mr-5 sticky top-0 text-black bg-gray-100/95 hidden lg:block'>
-            <div className='flex space-x-6 justify-end'>
-                <a
-                    className='px-2 py-1 rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
-                    href='#'
-                >
-                    About
-                </a>
-                <a
-                    className='px-2 py-1 rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
-                    href='#skills'
-                >
-                    Skills
-                </a>
-                <a
-                    className='px-2 py-1 rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
-                    href='#experience'
-                >
-                    Experience
-                </a>
-                <a
-                    className='px-2 py-1 rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
-                    href='#projects'
-                >
-                    Projects
-                </a>
-                <a
-                    className='px-2 py-1 rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
-                    href='#contact'
-                >
-                    Contact
-                </a>
-            </div>
-        </nav>
+        <header className='z-50 w-full fixed bottom-0 lg:sticky lg:top-0'>
+            <nav className='p-5 lg:mr-5 text-black bg-gray-50 lg:bg-gray-100/95 '>
+                <div className='flex justify-around md:justify-evenly lg:space-x-6 lg:justify-end'>
+                    <a
+                        className='text-gray-800 p-2 rounded-full lg:py-1 lg:rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
+                        href='#about'
+                    >
+                        <HiOutlineIdentification className='text-3xl lg:hidden' />
+                        <span className='hidden lg:block'>About</span>
+                    </a>
+                    <a
+                        className='text-gray-800 p-2 rounded-full lg:py-1 lg:rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
+                        href='#skills'
+                    >
+                        <HiOutlineCode className='text-3xl lg:hidden' />
+                        <span className='hidden lg:block'>Skills</span>
+                    </a>
+                    <a
+                        className='text-gray-800 p-2 rounded-full lg:py-1 lg:rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
+                        href='#experience'
+                    >
+                        <HiOutlineBriefcase className='text-3xl lg:hidden' />
+                        <span className='hidden lg:block'>Experience</span>
+                    </a>
+                    <a
+                        className='text-gray-800 p-2 rounded-full lg:py-1 lg:rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
+                        href='#projects'
+                    >
+                        <HiOutlineDocumentDuplicate className='text-3xl lg:hidden' />
+                        <span className='hidden lg:block'>Projects</span>
+                    </a>
+                    <a
+                        className='text-gray-800 p-2 rounded-full lg:py-1 lg:rounded-lg hover:text-white hover:bg-rose-500 active:bg-rose-400'
+                        href='#contact'
+                    >
+                        <HiOutlineChatAlt2 className='text-3xl lg:hidden' />
+                        <span className='hidden lg:block'>Contact</span>
+                    </a>
+                </div>
+            </nav>
+        </header>
     );
 };
 
